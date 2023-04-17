6250724c0bf6eb2f8d80555e4e143908a2f838fc
H-Seung <ggdd55440@gmail.com>
2023-04-17 10:03:44 +0900

    Overall modify: Add generator, etc.

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import argparse
 import sys
-import os
+import os, math
 import pandas as pd
 import time
 from tqdm import tqdm
@@ -23,19 +23,23 @@ from utils.trainData import *
 from utils.face_verifier2 import Verifier2
 
 
-def build_model(model, distance_metric):
+def build_model(model_name, distance_metric):
     """학습 모델 구성, x: 이미지 두 장, y: id, gender
     input: 
         기본 모델 이름
     output: 
         동일인 여부 예측 모델
     """
-    verifier = Verifier2(model, distance_metric)
+    verifier = Verifier2(model_name, distance_metric)
     return verifier.model
 # -----------------------------------
 
+# def create_df_image_paths(row):
+#     paths = df_paths['path'].apply(lambda x: os.path.join(row, x))
+#     return pd.DataFrame(paths)
 
-def train(df_path, img_path, model="vggface", distance_metric='cosine', batch_size=32, epochs=5, optimizer='adam', lr=0.001):
+
+def train(df_path, img_path, model_name="vggface", distance_metric='cosine', batch_size=32, epochs=3, optimizer='adam', lr=0.001):
     """모델 학습 함수
     input: 
         df_path : 학습시킬 이미지와 라벨 정보가 들어있는 엑셀파일 경로
@@ -46,56 +50,47 @@ def train(df_path, img_path, model="vggface", distance_metric='cosine', batch_si
         학습결과
     """
     set_seeds() # 시드 고정
-    # save_path = Path.joinpath(Path.cwd(), Path("models"))
-    save_path = os.path.join(ROOT, 'models', f'{model}-custom.hdf5') # 학습 가중치를 저장할 경로
-
-    ########### 가져올 데이터 수 설정 ###########
-    num_data = 1000
+    dir_path = "./weights"
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
 
     # Dataset 준비 -----------------
+    n_test = 10000 # 테스트용 ######################
 
     start = time.time()
     # 학습셋 정보 (이미지경로 및 라벨(image_path-id-gender) 엑셀) 읽어오기
-    df = get_label_data(df_path, num_data)  # 테스트용으로 일부 data 추출 (2개시트 각각 읽어오므로 2배)
+    df = get_label_data(df_path)  
+    df = df.sample(n=n_test, random_state=42) # 테스트용으로 일부 data 추출
     print("-> get_label_data time: ", time.time()-start)
 
     # train, test 데이터셋 split  -> (1376640,) (344160,)
-    X_train, X_test, y_train, y_test = train_test_split( 
-        df['File_Path'], df['ID'], test_size=0.2, stratify=df['Gender'], random_state=42)
-    print("X_train, X_test, y_train, y_test shape: ", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    train, test = train_test_split( 
+        df, test_size=0.2, stratify=df['Gender'], random_state=42)
+    train = train.reset_index(drop=True)
+    test = test.reset_index(drop=True)
+    print("train, test shape: ", train.shape, test.shape)
+    # print("X_train, X_test, y_train, y_test shape: ", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-    # pd.Series -> np.ndarray
-    if isinstance(X_train, pd.Series):
-        X_train, X_test, y_train, y_test = X_train.values, X_test.values, y_train.values, y_test.values
+    train_generator = generator(train, img_path, model_name, batch_size)
+    val_generator = generator(test, img_path, model_name, batch_size)
 
-    start = time.time()
-    # 이미지 경로 데이터를 array로 읽어오기 & 이미지 전처리
-    print("Converting image path data -> image arrays ...")
-    X_train = img_transform(X_train, img_path, model, batch_size)
-    X_test = img_transform(X_test, img_path, model, batch_size)
-    print("-> img_transform time: ", time.time()-start)
+    # # 스텝 설정
+    # steps_per_epoch = math.ceil(len(train) * NUM_PER_ID * 2 / batch_size)
+    # validation_steps = math.ceil(len(test) * NUM_PER_ID * 2 / batch_size)
 
-    start = time.time()
-    # 긍정/부정 이미지쌍 만들기
-    print("Creating image pairs ...")
-    pairImgTrain, pairLabelTrain = create_pairs(X_train, y_train, batch_size=batch_size, shuffle=True)
-    pairImgTest, pairLabelTest = create_pairs(X_test, y_test, batch_size=batch_size, shuffle=False)
+    run_options = tf.compat.v1.RunOptions()
+    run_options.report_tensor_allocations_upon_oom = True
 
-    print('pairImgTrain Shape :', pairImgTrain.shape)
-    print('pairLabelTrain Shape :', pairLabelTrain.shape)
-    print('pairImgTest Shape :', pairImgTest.shape)
-    print('pairLabelTest Shape :', pairLabelTest.shape)
-    print("-> create_pairs time: ", time.time()-start)
 
 
     # 모델 준비 -----------------
 
     # 학습할 모델 불러오기
-    model = build_model(model, distance_metric) 
+    model = build_model(model_name, distance_metric) 
     # model = load_model('VGG-Face') # 테스트
 
     # 손실 함수, 평가 지표 정의
-    loss = [get_contrastive_loss(margin=1), "binary_crossentropy"] # contrastive / 이진분류의 대표적 손실함수 
+    loss = ["binary_crossentropy"] 
     metrics = ["accuracy"]
     
     # optimizer 정의
@@ -119,35 +114,36 @@ def train(df_path, img_path, model="vggface", distance_metric='cosine', batch_si
     early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', 
                                                                 verbose=1,  # 콜백 메세지(0:출력X or 1:출력)
                                                                 patience=3)
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=save_path, 
-                                                                verbose=1,  
-                                                                save_best_only=True, 
-                                                                save_weights_only=True)
-    
-    # 모델 학습
-    print('---------- fit model ----------')
-    # history = model.fit(
-    #     [pairImgTrain[i][0] for i in range(len(pairImgTrain))],
-    #     [pairImgTrain[i][1] for i in range(len(pairImgTrain))],
-    #     pairLabelTrain,
-    #     epochs=epochs,
-    #     validation_data=([pairImgTest[i][0] for i in range(len(pairImgTest))],
-    #                      [pairImgTest[i][1] for i in range(len(pairImgTest))],
-    #                      pairLabelTest),
-    #     callbacks=[early_stopping_callback, checkpoint_callback],
-    # )
+    ## LambdaCallback 설정
+    keep_latest_weights_callback = tf.keras.callbacks.LambdaCallback(
+        on_epoch_end=lambda epoch, logs: keep_latest_n_weights(dir_path, n=10)
+    )
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath='weights/' + model_name + '_weights_epoch_{epoch:02d}-{accuracy:.3f}.h5',
+        save_weights_only=True,
+        monitor='val_loss',
+        mode='min',
+        save_best_only=False,
+        save_freq='epoch', # 1에폭마다 저장함
+        verbose=1,
+    )
+
+    print(f'---------- fit model ----------')
     history = model.fit(
-        [pairImgTrain[:,0], pairImgTrain[:,1]], pairLabelTrain,
+        train_generator,
+        validation_data=val_generator,
         epochs=epochs,
-        validation_data=([pairImgTest[:,0], pairImgTest[:,1]], pairLabelTest),
-        callbacks=[early_stopping_callback, checkpoint_callback],
-        batch_size=batch_size
+        callbacks=[checkpoint_callback, early_stopping_callback, keep_latest_weights_callback],
+        batch_size = batch_size,
+        verbose=1,
+        steps_per_epoch=len(train) // batch_size,
+        validation_steps= len(test) // batch_size
     )
 
     # 모델 평가 (나중에 main 함수 짜고 정리필요)
     plot_history(history)
     print('---------- evaluate model ----------')
-    results = model.evaluate([pairImgTest[:,0], pairImgTest[:,1]], pairLabelTest)
+    results = model.evaluate(val_generator)
     # 예측
     # print('---------- predict test set ----------')
     # predictions = model.predict([pairImgTest[:, 0], pairImgTest[:, 1]])
@@ -159,140 +155,19 @@ def train(df_path, img_path, model="vggface", distance_metric='cosine', batch_si
     # # best weight 불러오기
     # model.load_weights(save_path)
     
-    return history
-    # return (pairImgTrain, pairLabelTrain), (pairImgTest, pairLabelTest)
-# -----------------------------------
+    return history, results
 
 
-
-
-
-
-# def train(df_path, img_path, model="vggface", distance_metric='cosine', batch_size=32, epochs=5, optimizer='adam', lr=0.001):
-#     """모델 학습 함수
-#     input: 
-#         df_path : 학습시킬 이미지와 라벨 정보가 들어있는 엑셀파일 경로
-#         img_path : 이미지파일들이 저장되어 있는 상위 폴더 경로
-#         model : 가져올 모델 이름
-#         batch_size : 작게 설정할수록 학습에 시간이 더 오래 걸리지만, 메모리는 더 적게 쓸 수 있음
-#     output: 
-#         학습결과
-#     """
-#     set_seeds() # 시드 고정
-#     # save_path = Path.joinpath(Path.cwd(), Path("models"))
-#     save_path = os.path.join(ROOT, 'models', f'{model}-custom.hdf5') # 학습 가중치를 저장할 경로
-
-
-#     # Dataset 준비 -----------------
-
-#     start = time.time()
-#     # 학습셋 정보 (이미지경로 및 라벨(image_path-id-gender) 엑셀) 읽어오기
-#     df = get_label_data(df_path,1000)  # 테스트용으로 일부 data 추출 (2개시트 각각 읽어오므로 2배)
-#     print("-> get_label_data time: ", time.time()-start)
-
-#     # train, test 데이터셋 split  -> (1376640,) (344160,)
-#     X_train, X_test, y_train, y_test = train_test_split( 
-#         df['File_Path'], df['ID'], test_size=0.2, stratify=df['Gender'], random_state=42)
-#     print("X_train, X_test, y_train, y_test shape: ", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-
-#     # pd.Series -> np.ndarray
-#     if isinstance(X_train, pd.Series):
-#         X_train, X_test, y_train, y_test = X_train.values, X_test.values, y_train.values, y_test.values
-
-#     start = time.time()
-#     # 이미지 경로 데이터를 array로 읽어오기 & 이미지 전처리
-#     print("Converting image path data -> image arrays ...")
-#     X_train = img_transform(X_train, img_path, model, batch_size)
-#     X_test = img_transform(X_test, img_path, model, batch_size)
-#     print("-> img_transform time: ", time.time()-start)
-
-#     start = time.time()
-#     # 긍정/부정 이미지쌍 만들기
-#     print("Creating image pairs ...")
-#     pairImgTrain, pairLabelTrain = create_pairs(X_train, y_train, batch_size=batch_size, shuffle=True)
-#     pairImgTest, pairLabelTest = create_pairs(X_test, y_test, batch_size=batch_size, shuffle=False)
-
-#     print('pairImgTrain Shape :', pairImgTrain.shape)
-#     print('pairLabelTrain Shape :', pairLabelTrain.shape)
-#     print('pairImgTest Shape :', pairImgTest.shape)
-#     print('pairLabelTest Shape :', pairLabelTest.shape)
-#     print("-> create_pairs time: ", time.time()-start)
-
-
-#     # 모델 준비 -----------------
-
-#     # 학습할 모델 불러오기
-#     model = build_model(model, distance_metric) 
-#     # model = load_model('VGG-Face') # 테스트
-
-#     # 손실 함수, 평가 지표 정의
-#     loss = [get_contrastive_loss(margin=1), "binary_crossentropy"] # contrastive / 이진분류의 대표적 손실함수 
-#     metrics = ["accuracy"]
+def keep_latest_n_weights(dir_path, n=10):
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+        return
     
-#     # optimizer 정의
-#     if optimizer.upper() == 'ADAGRAD':
-#         opt = tf.keras.optimizers.Adagrad(learning_rate=lr)
-#     elif optimizer.upper() == 'ADADELTA':
-#         opt = tf.keras.optimizers.Adadelta(learning_rate=lr, rho=0.9, epsilon=1e-6)
-#     elif optimizer.upper() == 'ADAM':
-#         opt = tf.keras.optimizers.Adam(learning_rate=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-7)
-#     elif optimizer.upper() == 'RMSPROP':
-#         opt = tf.keras.optimizers.RMSprop(learning_rate=lr, rho=0.9, momentum=0.9, epsilon=1.0)
-#     elif optimizer.upper() == 'MOM':
-#         opt = tf.keras.optimizers.SGD(learning_rate=lr, momentum=0.9, nesterov=True)
-#     else:
-#         raise ValueError('Invalid optimization algorithm')
-    
-#     # 모델 컴파일
-#     model.compile(optimizer=opt, loss=loss, metrics=metrics)
+    weights_files = sorted([f for f in os.listdir(dir_path) if f.endswith('.h5')], 
+                           key=lambda x: os.path.getmtime(os.path.join(dir_path, x)), reverse=True)
+    for f in weights_files[n:]:
+        os.remove(os.path.join(dir_path, f))
 
-#     # callback 정의 - early stopping, model checkpointing
-#     early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', 
-#                                                                 verbose=1,  # 콜백 메세지(0:출력X or 1:출력)
-#                                                                 patience=5)
-#     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=save_path, 
-#                                                                 verbose=1,  
-#                                                                 save_best_only=True, 
-#                                                                 save_weights_only=True)
-    
-#     # 모델 학습
-#     print('---------- fit model ----------')
-#     # history = model.fit(
-#     #     [pairImgTrain[i][0] for i in range(len(pairImgTrain))],
-#     #     [pairImgTrain[i][1] for i in range(len(pairImgTrain))],
-#     #     pairLabelTrain,
-#     #     epochs=epochs,
-#     #     validation_data=([pairImgTest[i][0] for i in range(len(pairImgTest))],
-#     #                      [pairImgTest[i][1] for i in range(len(pairImgTest))],
-#     #                      pairLabelTest),
-#     #     callbacks=[early_stopping_callback, checkpoint_callback],
-#     # )
-#     history = model.fit(
-#         [pairImgTrain[:,0], pairImgTrain[:,1]], pairLabelTrain,
-#         epochs=epochs,
-#         validation_data=([pairImgTest[:,0], pairImgTest[:,1]], pairLabelTest),
-#         callbacks=[early_stopping_callback, checkpoint_callback],
-#         batch_size=batch_size
-#     )
-
-#     # 모델 평가 (나중에 main 함수 짜고 정리필요)
-#     plot_history(history)
-#     print('---------- evaluate model ----------')
-#     results = model.evaluate([pairImgTest[:,0], pairImgTest[:,1]], pairLabelTest)
-#     # 예측
-#     # print('---------- predict test set ----------')
-#     # predictions = model.predict([pairImgTest[:, 0], pairImgTest[:, 1]])
-#     # # 테스트 쌍 예측 결과 시각화
-#     # pair_plot(pairImgTest, pairIdTest, pairSexTest, to_show=21, predictions=predictions, test=True)
-#     # cm_plot(pairIdTest, predictions[0], 'face')
-
-    
-#     # # best weight 불러오기
-#     # model.load_weights(save_path)
-    
-#     return history
-#     # return (pairImgTrain, pairLabelTrain), (pairImgTest, pairLabelTest)
-# # -----------------------------------
 
 
 
@@ -303,7 +178,10 @@ img_path = '../DATA_AIHub/dataset/'
 # print("train_dataset[0](Img), train_dataset[1](Label) shape:",train_dataset[0].shape, train_dataset[1].shape)
 # print("val_dataset[0](Img), val_dataset[1](Label) shape:",val_dataset[0].shape, val_dataset[1].shape)
 
-print(train(data_path, img_path))
+history, val_result = train(data_path, img_path, batch_size=8)
+print(history)
+print()
+print(val_result)
 
 
 
--- utils/trainData.py
+++ utils/trainData.py
@@ -5,6 +5,7 @@ import cv2
 import random
 from tqdm import tqdm
 from pathlib import Path
+import gc
 from utils.function.generals import *
 from utils.face_detector import FacePreparer
 
@@ -28,11 +29,11 @@ def get_label_data(df_path, nrows=None):
 # -----------------------------------
 
 
-def img_transform(img_path_arr, img_base_path, model, batch_size=32):
+def img_transform(img_path, img_base_path, model_name):
     """ batch_size 단위로 이미지 경로를 array로 읽어오고 
         얼굴 부분만 크롭, 패딩, 리사이즈의 이미지 전처리를 수행
     input: 
-        img_path_arr : 이미지 경로들의 array
+        img_path : 이미지 경로 str 
         img_base_path : 이미지파일들이 저장되어 있는 상위 폴더 경로
         model : 사용하는 모델 이름
         batch_size : 한 번에 처리할 데이터 수
@@ -40,83 +41,79 @@ def img_transform(img_path_arr, img_base_path, model, batch_size=32):
         이미지를 (데이터 수, target_size[0], target_size[1], 채널수) 형태의 np.ndarray로 반환
     """
     preparer = FacePreparer()
-    target_size = find_target_size(model)
     img_sets = []
-    for i in tqdm(range(0, len(img_path_arr), batch_size)):
-        batch_paths = img_path_arr[i:i+batch_size]
-        batch_imgs = []
-        for img_path in batch_paths:
-            img = cv2.imread(str(Path(os.path.join(img_base_path, img_path))))
-            faces = preparer.detect_faces(img, model, align=False)  # 얼굴 탐지, 크롭, 패딩, 리사이즈
-            if len(faces) > 0: 
-                img = faces[0]/255.0 # 이미지 리스트에서 감지된 얼굴 하나 선택 & 정규화
-            else:
-                img = "None"
-                # print("얼굴이 검출되지 않았습니다 -> ", img_path)
-            batch_imgs.append(img)
-        img_sets.extend(np.array(batch_imgs))
+    
+    img = cv2.imread(str(Path(os.path.join(img_base_path, img_path))))
+    faces = preparer.detect_faces(img, model_name, align=False)  # 얼굴 탐지, 크롭, 패딩, 리사이즈
+    if len(faces) > 0: 
+        img = faces[0]/255.0 # 이미지 리스트에서 감지된 얼굴 하나 선택 & 정규화
+    else:
+        img = None
+        # print("얼굴이 검출되지 않았습니다 -> ", path)
 
-    return img_sets
+    return img  # array([[[ ]]]) / None
 # -----------------------------------
 
 
-def create_pairs(X, y, batch_size=32, shuffle=True): #클래스에 대해 반복
+def generator(df, base_path, model_name, generator_batch= 32): #클래스에 대해 반복
     """ 동일인여부 예측을 위해 긍정/부정 이미지 쌍을 만들어주는 함수
     input: 
         이미지 list, id array를 입력
     output: 
         ((이미지, 이미지), 동일인여부 라벨)
     """
-    pairImages = []  # (이미지, 이미지) 쌍
-    pairLabels = [] # 긍정(두 사진이 동일인):1, 부정(두 사진이 비동일인):0 레이블
+    X = df["File_Path"]
+    y = df["ID"]
+
+    # print("Creating image pairs ...")
+    pair_source_list = [] 
+    pair_target_list = []
+    pair_label_list = [] # 긍정(두 사진이 동일인):1, 부정(두 사진이 비동일인):0 레이블
     unique_labels = np.unique(y) # id 클래스
+    random_unique_labels = np.random.choice(unique_labels, size=50, replace=False)
 
-    # 모든 클래스에 대해 반복
-    for label in unique_labels:
-        pos_indices = np.where(y == label)[0]  # id가 같은 데이터 인덱스들
-        neg_indices = np.where(y != label)[0]
-        n_samples = len(pos_indices) if len(pos_indices)<len(neg_indices) else len(neg_indices)
-        for i in range(n_samples//2):
-            img_idx_1 = pos_indices[i*2]  # 긍정 짝수번째의 인덱스
-            img_idx_2 = pos_indices[i*2+1]  # 긍정 홀수번째의 인덱스
-            img_idx_3 = neg_indices[i*2] # 부정 인덱스
-            if isinstance(X[img_idx_1], str) or isinstance(X[img_idx_2], str) or isinstance(X[img_idx_3], str): 
-                print("img pair skip - ", y[img_idx_1], isinstance(X[img_idx_1], str),", ", y[img_idx_2], isinstance(X[img_idx_2], str),", ", y[img_idx_3], isinstance(X[img_idx_3], str))
-                continue # 얼굴이 검출되지 않은 경우 다음으로 (=사진 제외)
-            # 동일 id들의 인덱스들에서 긍정쌍 생성
-            pairImages.append([X[img_idx_1], X[img_idx_2]])
-            pairLabels.append(1)
-            # 다른 id들의 인덱스와 부정쌍 생성
-            pairImages.append([X[img_idx_1], X[img_idx_2]])
-            pairLabels.append(0)
-    
-    pairLabels = np.array(pairLabels, dtype=np.uint8)
-    pairImages = np.array(pairImages, dtype=np.uint8) # 여기서 메모리 문제
 
-    ########
-    # num_pairs = len(pairImages)
-    # # 리스트를 배치 단위로 분할하여 array로 변환 후 합치기
-    # pairImages_list = [pairImages[i:i+batch_size] for i in range(0, num_pairs, batch_size)]
-    # pairLabels_list = [pairLabels[i:i+batch_size] for i in range(0, num_pairs, batch_size)]
-
-    # pairImages = np.concatenate([np.array(batch, dtype=np.uint8) for batch in pairImages_list])
-    # pairLabels = np.concatenate([np.array(batch, dtype=np.uint8) for batch in pairLabels_list])
-    ########
-
-    if shuffle:
-        indices = np.arange(len(pairLabels))
-        np.random.shuffle(indices)
-        pairImages = pairImages[indices]
-        pairLabels = pairLabels[indices]
-
-    # # TensorFlow Dataset 객체 생성
-    # dataset = tf.data.Dataset.from_tensor_slices((pairImages, pairLabels))
-    # # preprocess pairs
-    # dataset = dataset.map(lambda x, y: ((img_transform(x[0]), img_transform(x[1])), y))
-    # # batch and prefetch
-    # dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    while True:
+        # 모든 클래스에 대해 반복
+        for label in random_unique_labels:
+            if int(np.where(random_unique_labels==label)[0]+1) > len(random_unique_labels)-1:  # 마지막 label 실행X
+                return
+            print(f"\tlabel:{label} ({int(np.where(random_unique_labels==label)[0]+1)}/{len(random_unique_labels)})")
+            gc.collect()
+            pos_indices = np.where(y == label)[0]  # id가 같은 데이터 인덱스들
+            neg_indices = np.where(y != label)[0]
+            n_samples = len(pos_indices) if len(pos_indices)<len(neg_indices) else len(neg_indices)
+            iter_n = min(n_samples//2, 500)
+            
+            for i in range(iter_n):
+                img_idx_1 = pos_indices[i*2]  # 긍정 짝수번째의 인덱스
+                img_idx_2 = pos_indices[i*2+1]  # 긍정 홀수번째의 인덱스
+                img_idx_3 = neg_indices[i*2] # 부정 인덱스
+                # 이미지 배열 읽어오기
+                source_img = img_transform(X[img_idx_1], base_path, model_name)
+                target_img = img_transform(X[img_idx_2], base_path, model_name)
+                neg_target_img = img_transform(X[img_idx_3], base_path, model_name)
+                if (source_img is None) or (target_img is None) or (neg_target_img is None): 
+                    # print("img pair skip - ", y[img_idx_1], source_img is None,", ", y[img_idx_2], target_img is None,", ", y[img_idx_3], neg_target_img is None)
+                    continue # 얼굴이 검출되지 않은 경우 다음으로 (=사진 제외)
+                # 동일 id들의 인덱스들에서 긍정쌍 생성
+                pair_source_list.append(source_img)
+                pair_target_list.append(target_img)
+                pair_label_list.append(1)
+                # 다른 id들의 인덱스와 부정쌍 생성
+                pair_source_list.append(source_img)
+                pair_target_list.append(neg_target_img)
+                pair_label_list.append(0)
+                if len(pair_label_list) >= generator_batch: # generator_batch 이상이 되면 
+                    pair_source_array = np.array(pair_source_list, dtype=np.float32)
+                    pair_source_list.clear()
+                    pair_target_array = np.array(pair_target_list, dtype=np.float32)
+                    pair_target_list.clear()
+                    pair_label_array = np.array(pair_label_list, dtype=np.uint8)
+                    pair_label_list.clear()
+                    # print("pair_source_array, pair_target_array, pair_label_array shape: ", pair_source_array.shape, pair_target_array.shape, pair_label_array.shape)
+                    yield [pair_source_array, pair_target_array], pair_label_array
         
-    # return dataset
-    return pairImages, pairLabels
 
 
+                    
